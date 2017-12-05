8e0e84c1a2fcd366739105a84822473f78be2d9f
Unknown <delea.vanreeth@gmail.com>
2017-12-05 12:09:32 +0100

    Excercises 1-2 and 1-3

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -1,8 +1,10 @@
 public class Hello {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         // generate some simple output
         System.out.println("Hello, World!");
+        // printing second line
+        System.out.println("How are you?");
     }
 
 }
